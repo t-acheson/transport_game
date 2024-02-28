5d7fd9d67eed144b056203620e968a475cd4c928
mhmckeon <147508900+mhmckeon@users.noreply.github.com>
2024-02-28 14:00:24 +0000

    Add constructors to Entity class, and update Gem class to use Entity constructor - fixed rendering in bottom left corner

--- core/src/com/hotmomcircle/transport_game/entity/Gem.java
+++ core/src/com/hotmomcircle/transport_game/entity/Gem.java
@@ -9,17 +9,23 @@ public class Gem extends Entity {
     private Texture gemImage;
 
     public Gem(int x, int y) {
+        super(x, y);
         Rectangle gem = new Rectangle();
-        gem.x = x;
-        gem.y = y;
+        gem.x = this.getX();
+        gem.y = this.getY();
         gem.width = 32;
         gem.height = 32;
         gemImage = new Texture(Gdx.files.internal("gem.png"));
     }
 
-    
 
     public void render(SpriteBatch batch) {
         batch.draw(gemImage, this.getX(), this.getY());
+    }
+
+    public static void main(String[] args) {
+        Gem gem = new Gem(150, 150);
+        System.out.println(gem.getX());
+        System.out.println(gem.getY());
     }
 }
