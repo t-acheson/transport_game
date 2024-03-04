fd78da574bb6a75c265c059e6ca6110c809e09c5
mhmckeon <147508900+mhmckeon@users.noreply.github.com>
2024-03-04 19:37:43 +0000

    pause functionality implemented - need to tidy still

--- core/src/com/hotmomcircle/transport_game/GameScreen.java
+++ core/src/com/hotmomcircle/transport_game/GameScreen.java
@@ -1,6 +1,7 @@
 package com.hotmomcircle.transport_game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -10,7 +11,7 @@ import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.hotmomcircle.transport_game.entity.Gem;
 import com.hotmomcircle.transport_game.entity.Player;
-
+import com.hotmomcircle.transport_game.screen.PauseScreen;
 //map imports below 
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
@@ -36,31 +37,41 @@ public class GameScreen implements Screen {
 	
 	Texture img;
 	public Player player;
-
+	
 	public Array<Gem> gems;
-	   
-   private OrthographicCamera camera;
-
+	
+    private OrthographicCamera camera;
+	
+	private int GAME_STATE;
+	
+	private final int GAME_RUNNING = 0;
+	private final int GAME_PAUSED = 1;
+	PauseScreen pauseScreen;
+	
+	
 	
 	public GameScreen(TransportGame game) {
 		this.game = game;
 		
 		this.batch = game.batch;
 		
+		// for the pause / play feature
+		GAME_STATE = GAME_RUNNING;
+		
 		//loading map 
 		TmxMapLoader loader = new TmxMapLoader();
 		try {
-			 map = loader.load("trialMap.tmx");
+			map = loader.load("trialMap.tmx");
 			System.out.println("Map loaded successfully.");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		renderer = new OrthogonalTiledMapRenderer(map);	
 		//
-
+		
 		
 		player = new Player(this);
-
+		
 		gems = new Array<Gem>();
 		gems.add(new Gem(100, 100));
 		gems.add(new Gem(200, 200));
@@ -80,45 +91,61 @@ public class GameScreen implements Screen {
 
 	@Override
 	public void render(float delta) {
-		ScreenUtils.clear(0, 0, 0.2f, 1);
+
+
+		if(Gdx.input.isKeyPressed(Input.Keys.P) && GAME_STATE != GAME_PAUSED) {
+			pause();
+		} 
+		if(Gdx.input.isKeyPressed(Input.Keys.R) && GAME_STATE != GAME_RUNNING) {
+			resume();
+		} 
+		if (GAME_STATE == GAME_PAUSED){
+			// draw the pause screen
+			
+
+		} else {
+
 		
-		// map render 
-		renderer.setView(camera);
-		renderer.render();
-		//
+			ScreenUtils.clear(0, 0, 0.2f, 1);
+			
+			// map render 
+			renderer.setView(camera);
+			renderer.render();
+			//
 
-		for (Gem gem : gems) {
-			if (player.getPlayerRectangle().overlaps(gem.getGemRectangle())) {
-				gem.dispose();
-				gems.removeValue(gem, true);
-			}
-		}
-
-		// TODO Auto-generated method stub
-      // clear the screen with a dark blue color. The
-      // arguments to clear are the red, green
-      // blue and alpha component in the range [0,1]
-      // of the color to be used to clear the screen.
-
-      // tell the camera to update its matrices.
-      camera.update();
-
-      // tell the SpriteBatch to render in the
-      // coordinate system specified by the camera.
-      batch.setProjectionMatrix(camera.combined);
-
-		// ScreenUtils.clear(1, 0, 0, 1); 
-		batch.begin();
-		try {
-			player.render(batch);
 			for (Gem gem : gems) {
-				gem.render(batch);
+				if (player.getPlayerRectangle().overlaps(gem.getGemRectangle())) {
+					gem.dispose();
+					gems.removeValue(gem, true);
+				}
 			}
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+			// TODO Auto-generated method stub
+		// clear the screen with a dark blue color. The
+		// arguments to clear are the red, green
+		// blue and alpha component in the range [0,1]
+		// of the color to be used to clear the screen.
+
+		// tell the camera to update its matrices.
+		camera.update();
+
+		// tell the SpriteBatch to render in the
+		// coordinate system specified by the camera.
+		batch.setProjectionMatrix(camera.combined);
+
+			// ScreenUtils.clear(1, 0, 0, 1); 
+			batch.begin();
+			try {
+				player.render(batch);
+				for (Gem gem : gems) {
+					gem.render(batch);
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			batch.end();
 		}
-		batch.end();
 		
 	}
 
@@ -131,13 +158,15 @@ public class GameScreen implements Screen {
 	@Override
 	public void pause() {
 		// TODO Auto-generated method stub
+		System.out.println("Game Paused");
+		GAME_STATE = GAME_PAUSED;
 		
 	}
 
 	@Override
 	public void resume() {
 		// TODO Auto-generated method stub
-		
+		GAME_STATE = GAME_RUNNING;
 	}
 
 	@Override
