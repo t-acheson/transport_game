11800f18abe8e9c3babf5a36d35e3c95dcbd70c4
eoin m treacy <eointreacy5@gmail.com>
2024-03-05 12:35:37 +0000

    increment/decrement carbon and freshness based on player move

--- core/src/com/hotmomcircle/transport_game/GameScreen.java
+++ core/src/com/hotmomcircle/transport_game/GameScreen.java
@@ -24,73 +24,73 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
-
- 
 // This will be the screen 
 public class GameScreen implements Screen {
-	
+
 	TransportGame game;
-	
-	
+
 	SpriteBatch batch;
-	
+
 	private int originalTileSize = 16;
 	public int scale = 2;
 	private int tileSize = originalTileSize * scale;
-	
-	//initalising map 
+
+	// initalising map
 	private TiledMap map;
 	private OrthogonalTiledMapRenderer renderer;
-	
+
 	Texture img;
 	public Player player;
 
 	public Array<Gem> gems;
-	   
-   private OrthographicCamera camera;
 
-   // Stage for UI components 
-   private Stage stage;
-   private Table table;
-   private Points points;
-   private Points carbon;
-   private Points freshness;
+	private OrthographicCamera camera;
 
-   // asset manager to implement uiskin.json
-   // TODO best practise to implement all our assets this way? 
-   private AssetManager assetManager;
+	// Stage for UI components
+	private Stage stage;
+	private Table table;
+
+	// scores need to be public so Player can modify
+	public Points points;
+	public Points carbon;
+	public Points freshness;
+
+	// asset manager to implement uiskin.json
+	// TODO best practise to implement all our assets this way?
+	private AssetManager assetManager;
 
 	public GameScreen(TransportGame game) {
 		this.game = game;
-		
+
 		this.batch = game.batch;
-		
-		//loading map 
+
+		// loading map
 		TmxMapLoader loader = new TmxMapLoader();
 		try {
-			 map = loader.load("trialMap.tmx");
+			map = loader.load("trialMap.tmx");
 			System.out.println("Map loaded successfully.");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		renderer = new OrthogonalTiledMapRenderer(map);	
+		renderer = new OrthogonalTiledMapRenderer(map);
 		//
 
-		
 		player = new Player(this);
 
 		gems = new Array<Gem>();
 		gems.add(new Gem(400, 400));
 		gems.add(new Gem(200, 200));
 		gems.add(new Gem(300, 300));
-		
+
 		// create the camera and the SpriteBatch
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, game.SCREEN_WIDTH, game.SCREEN_HEIGHT);
-		
-		// TODO explain stages, skins, assetmanager
-		stage = new Stage(new ScreenViewport()); 
-        Gdx.input.setInputProcessor(stage);
+
+		// Stage is the layer on which we draw the UI
+		// Likely keeps things cleaner as we make
+		// the map more complicated and add objects
+		stage = new Stage(new ScreenViewport());
+		Gdx.input.setInputProcessor(stage);
 		Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
 
 		// Asset manager instansiation
@@ -100,8 +100,8 @@ public class GameScreen implements Screen {
 		// table to hold UI elements
 		table = new Table();
 		table.setFillParent(true);
-		table.defaults().width(game.SCREEN_WIDTH/6).expandX().fillX();
-		table.setWidth(game.SCREEN_WIDTH/6);
+		table.defaults().width(game.SCREEN_WIDTH / 6).expandX().fillX();
+		table.setWidth(game.SCREEN_WIDTH / 6);
 		table.left().top();
 
 		// UI scores
@@ -118,20 +118,21 @@ public class GameScreen implements Screen {
 		table.add(freshness).fillX().uniformX();
 
 		// add table to stage
-        stage.addActor(table);
-		
+		stage.addActor(table);
+
 	}
+
 	@Override
 	public void show() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void render(float delta) {
 		ScreenUtils.clear(0, 0, 0.2f, 1);
-		
-		// map render 
+
+		// map render
 		renderer.setView(camera);
 		renderer.render();
 		//
@@ -145,23 +146,23 @@ public class GameScreen implements Screen {
 		}
 
 		// TODO Auto-generated method stub
-      // clear the screen with a dark blue color. The
-      // arguments to clear are the red, green
-      // blue and alpha component in the range [0,1]
-      // of the color to be used to clear the screen.
+		// clear the screen with a dark blue color. The
+		// arguments to clear are the red, green
+		// blue and alpha component in the range [0,1]
+		// of the color to be used to clear the screen.
 
-      // tell the camera to update its matrices.
-      camera.update();
+		// tell the camera to update its matrices.
+		camera.update();
 
-		// UI draw 
+		// UI draw
 		stage.act(delta);
 		stage.draw();
 
-      // tell the SpriteBatch to render in the
-      // coordinate system specified by the camera.
-      batch.setProjectionMatrix(camera.combined);
+		// tell the SpriteBatch to render in the
+		// coordinate system specified by the camera.
+		batch.setProjectionMatrix(camera.combined);
 
-		// ScreenUtils.clear(1, 0, 0, 1); 
+		// ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
 		try {
 			player.render(batch);
@@ -173,45 +174,45 @@ public class GameScreen implements Screen {
 			e.printStackTrace();
 		}
 		batch.end();
-		
+
 	}
 
 	@Override
 	public void resize(int width, int height) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void pause() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void resume() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void hide() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
-		
-		// TODO check if right place for this disposal 
+
+		// TODO check if right place for this disposal
 		map.dispose();
 		renderer.dispose();
 		stage.dispose();
 		assetManager.dispose();
-		
+
 	}
-	
+
 	public int getTileSize() {
 		return tileSize;
 	}
