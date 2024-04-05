799d5b772dd16167b4d02bf9766fcf86420dffb2
mhmckeon <147508900+mhmckeon@users.noreply.github.com>
2024-04-05 11:11:15 +0100

    work in progress

--- core/src/com/hotmomcircle/transport_game/GameScreen.java
+++ core/src/com/hotmomcircle/transport_game/GameScreen.java
@@ -34,6 +34,7 @@ import com.hotmomcircle.transport_game.object.Car_OBJ;
 import com.hotmomcircle.transport_game.object.Transport_OBJ;
 import com.hotmomcircle.transport_game.entity.Route;
 import com.hotmomcircle.transport_game.tools.Camera;
+import com.hotmomcircle.transport_game.tools.WorldMap;
 import com.hotmomcircle.transport_game.entity.Node;
 import com.hotmomcircle.transport_game.ui.Planning;
 import com.hotmomcircle.transport_game.ui.Points;
@@ -74,13 +75,9 @@ public class GameScreen implements Screen, Json.Serializable {
 	   
 	public Camera camera;
 	// for the world map on press of "M"
-	public OrthographicCamera worldMap;
-	// Rectangle currentScreenMarker;
 	boolean showWorldMap = false;
-	ShapeRenderer shape;
-	double panSpeed = 250;
-	int mapWidthInPixels;
-	int mapHeightInPixels;
+	WorldMap worldMap;
+	
 	// Texture playerMap = new Texture("assets/phoneScreen.png");
 	
 	public Array<Gem> gems;
@@ -218,20 +215,6 @@ public class GameScreen implements Screen, Json.Serializable {
 
 		// initialise Node array
 		nodes = new Array<Node>();
-
-		// for world map
-		TiledMapTileLayer worldMapLayer = (TiledMapTileLayer) map.getLayers().get(1);
-		int mapWidth = worldMapLayer.getWidth();
-		int tileWidth = (int) worldMapLayer.getTileWidth();
-		mapWidthInPixels = mapWidth * tileWidth*3;
-
-		int mapHeightInTiles = worldMapLayer.getHeight();
-		int tileHeight = (int) worldMapLayer.getTileHeight();
-		int mapHeightInPixels = mapHeightInTiles * tileHeight*3;
-
-		
-		worldMap = new OrthographicCamera();
-		worldMap.setToOrtho(false, mapWidthInPixels, mapHeightInPixels);
 		
 
 		for (MapLayer layer : map.getLayers()) {
@@ -316,7 +299,11 @@ public class GameScreen implements Screen, Json.Serializable {
 
 		pauseUI = new Pause(game, this, pauseStage, skin);
 
-		shape = new ShapeRenderer();
+		worldMap = new WorldMap(OrthogonalTiledMapRenderer renderer,);
+
+		
+
+		
 	}
 
 	@Override
@@ -355,69 +342,7 @@ public class GameScreen implements Screen, Json.Serializable {
 		}
 
 		else if (showWorldMap) {
-			worldMap.zoom = MathUtils.clamp(worldMap.zoom, 0.1f, 1f);
-
-			if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-				worldMap.zoom -= 0.02; // Zoom in
-				panSpeed -= 250;
-			}
-			
-			if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-				worldMap.zoom += 0.02; // Zoom out
-				panSpeed += 250;
-			}
-			if (Gdx.input.isKeyPressed(Input.Keys.D)) {
-				worldMap.translate((float) (-panSpeed * Gdx.graphics.getDeltaTime()), 0);
-			}
-			if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-				worldMap.translate((float)(panSpeed * Gdx.graphics.getDeltaTime()), 0);
-			}
-			if (Gdx.input.isKeyPressed(Input.Keys.S)) {
-				worldMap.translate(0, (float)(panSpeed * Gdx.graphics.getDeltaTime()));
-			}
-			if (Gdx.input.isKeyPressed(Input.Keys.W)) {
-				worldMap.translate(0, (float) (-panSpeed * Gdx.graphics.getDeltaTime()));
-			}
-			// Calculate the edges of the worldMap view in world coordinates
-			float halfCamWidth = worldMap.viewportWidth * worldMap.zoom * 0.5f;
-			float halfCamHeight = worldMap.viewportHeight * worldMap.zoom * 0.5f;
-
-			// Calculate the minimum and maximum x and y values the worldMap can have
-			float minX = halfCamWidth;
-			float maxX = mapWidthInPixels - halfCamWidth;
-			float minY = halfCamHeight;
-			float maxY = 8000 - halfCamHeight;
-
-			// Clamp the worldMap's position
-			worldMap.position.x = Math.max(minX, Math.min(maxX, worldMap.position.x));
-			worldMap.position.y = Math.max(minY, Math.min(maxY, worldMap.position.y));
-
-			
-			worldMap.update();
-			renderer.setView(worldMap);
-			batch.setProjectionMatrix(worldMap.combined);
-			renderer.render();
-			batch.begin();
-			try{
-				// batch.draw(playerMap,0,0);
-				batch.draw(player.image, player.getX(),player.getY(), 256,256);
-				for (Gem gem : gems) {
-					batch.draw(gem.image, gem.getX(), gem.getY(), 750,750);
-				}
-				
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			batch.end();
-
-			shape.setProjectionMatrix(worldMap.combined);
-
-			shape.begin(ShapeRenderer.ShapeType.Line);
-
-        	shape.setColor(Color.RED);
-			shape.rect((player.getX()-(camera.viewportWidth/2)), (player.getY()-(camera.viewportHeight/2)), camera.viewportWidth, camera.viewportHeight);
-			shape.end();
+			worldMap.render();
 
 
 		} else {
