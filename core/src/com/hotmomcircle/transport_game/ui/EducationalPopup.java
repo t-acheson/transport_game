1d9591b109ab62cfe45be241294f30020ecf5eb1
mhmckeon <147508900+mhmckeon@users.noreply.github.com>
2024-04-15 11:25:34 +0100

    started educational pop up work

---
+++ core/src/com/hotmomcircle/transport_game/ui/EducationalPopup.java
@@ -0,0 +1,13 @@
+package com.hotmomcircle.transport_game.ui;
+
+public class EducationalPopup {
+    private boolean bike;
+    private boolean bus;
+    private boolean car;
+    private boolean walking;
+    private boolean luas;
+    private boolean gem;
+
+    
+
+}
