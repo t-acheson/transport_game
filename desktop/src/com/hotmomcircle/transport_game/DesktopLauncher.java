4d2f468523cbe1aac3762e4fd0e81f94ab9c62e3
Daniel O'Flynn <danielpoflynn@gmail.com>
2024-04-23 21:39:15 +0100

    scale window

--- desktop/src/com/hotmomcircle/transport_game/DesktopLauncher.java
+++ desktop/src/com/hotmomcircle/transport_game/DesktopLauncher.java
@@ -8,7 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setWindowedMode(1600, 960);
+		config.setWindowedMode(1024, 768);
 		config.setResizable(false);
 		config.useVsync(true);
 		config.setTitle("Travel City");
